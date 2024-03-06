a347fb98cd047dbae88db70778a22a43c745e6f6
Adrien <adrien.abescat@agenium.com>
2024-03-06 12:58:47 +0100

    tuto 2

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -1,14 +1,42 @@
 package org.example;
 
+import org.example.inputs.KeyboardInputs;
+import org.example.inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
     public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(100, 100, 200, 50);
+        g.fillRect(xDelta, yDelta, 200, 50);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPosition(int x, int y){
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
     }
 }
